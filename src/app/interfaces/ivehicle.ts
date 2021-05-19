81411a3cbb64c37fe65599909eefd340608367ea
Tiago Batista Correa <tiagobc@live.com>
2021-05-19 00:30:36 -0300

    Incremento visual na home.

--- src/app/interfaces/ivehicle.ts
+++ src/app/interfaces/ivehicle.ts
@@ -1,2 +1,9 @@
-export interface Ivehicle {
+export interface IvehicleBasic {
+    id:string,
+    placa:string,
+    chassi:string,
+    renavam:string,
+    modelo:string,
+    marca:string,
+    ano:number
 }
